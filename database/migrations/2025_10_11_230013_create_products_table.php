22d1df0b8108cb65249333af14c05963be0ef490
LeoJuarez-senati <1520066@senati.pe>
2025-10-13 17:29:29 -0500

    "Primera parte hecha"

--- database/migrations/2025_10_11_230013_create_products_table.php
+++ database/migrations/2025_10_11_230013_create_products_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('sku')->unique();
+            $table->unsignedInteger('stock')->default(0);
+            $table->decimal('price',10,2);
+            $table->boolean('is_active')->default(true);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
